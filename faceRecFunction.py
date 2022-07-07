e89d751ce632c2aabede0c2737cf913a24808b26
chris190698 <57401427+chris190698@users.noreply.github.com>
2022-07-07 15:24:45 +0200

    bugfix

--- faceRecFunction.py
+++ faceRecFunction.py
@@ -4,16 +4,40 @@ import time
 import os
 from ctypes import CDLL
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # Oggetto del face detector
+
+'''
+Funzione che taglia alcuni parti della foto, permettendo all'algoritmo di operare solo con quelle più rilevanti.
+Limite: necessario registrare il volto in maniera frontale
+:param image: immagine da tagliare
+:param faces_coord: cordinate per delimitare il viso
+:return faces: coordinate del volto escluso le parti laterali
+'''
+def cut_faces(image, faces_coord) -> list:
+
+    faces = []
+    for (x, y, w, h) in faces_coord:  # Ritaglia parti del viso
+
+        w_rm = int(0.2 * w / 2)
+        faces.append(image[y: y + h, x + w_rm:  x + w - w_rm])
+
+    return faces
+
 '''
 Funzione che permette l'aggiunta di nuovi utenti al dataset creando opportunamento le directory per ognuno
-@:param None
-@:return None
+:param None
+:return None
 '''
 def add_person() -> None:
 
     # prende in input  il nome della persona che si vuole registrare
     person_name = input('Inserisci il nome della persona: ').lower()
-    folder = 'people_folder' + '/' + person_name
+    peopleFolder = 'people_folder'
+    folder = peopleFolder + '/' + person_name
+
+    if not os.path.exists(peopleFolder):
+
+        os.mkdir(peopleFolder)
 
     # Controlla che non sia presente già una cartella con lo stesso nome
     if not os.path.exists(folder):
@@ -22,36 +46,32 @@ def add_person() -> None:
         os.mkdir(folder)  # Crea un nuovo folder per salvare le foto
         video = cv2.VideoCapture(0)
         detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # Carica l'Haarcascade per identificare il volto.
-        timer = 0
         cv2.namedWindow('Video Feed', cv2.WINDOW_AUTOSIZE)
         cv2.namedWindow('Saved Face', cv2.WINDOW_NORMAL)
 
         #viene avviato il ciclo per scattare le 20 foto
-        for counter in range(20):
+        for counter in range(21):
 
             _, frame = video.read()
             if counter == 1:
+
                 time.sleep(6)
+
             else:
+
                 time.sleep(1)
 
-            faces = detector.detectMultiScale(frame)  # Trova le coordinate di tutte le facce nel frame
+            faces = detector.detectMultiScale(frame)  # Trova le coordinate di tutta le faccia nel frame
 
-            if len(faces):  # Se abbiamo alcuni volti
+            if len(faces):  # Se sono state identificate le coordinate
 
                 cut_face = cut_faces(frame, faces)  # Rimuove le parti inutili del volto
-
                 face_bw = cv2.cvtColor(cut_face[0], cv2.COLOR_BGR2GRAY)
-
                 face_bw_eq = cv2.equalizeHist(face_bw)  # Equalizzazione Istogramma
-                face_bw_eq = cv2.resize(face_bw_eq, (100, 100),
-                                        interpolation=cv2.INTER_CUBIC)  # Effettua un Resizing dell'immagine in 100x100 pixel
+                face_bw_eq = cv2.resize(face_bw_eq, (100, 100), interpolation=cv2.INTER_CUBIC)  # Effettua un Resizing dell'immagine in 100x100 pixel
                 # cv2.imshow('Face Recogniser', face_bw_eq)
-
-                cv2.imwrite(folder + '/' + str(counter) + '.jpg',
-                            face_bw_eq)
+                cv2.imwrite(folder + '/' + str(counter) + '.jpg', face_bw_eq)
                 print('Images Saved:' + str(counter))
-                counter += 1
                 cv2.imshow('Saved Face', face_bw_eq)  # Mostra la faccia che è stata salvata
 
             cv2.imshow('Video Feed', frame)
@@ -61,3 +81,85 @@ def add_person() -> None:
 
         # Viene visualizzato un messaggi di errore in caso il folder sia già presente
         print("This name already exists.")
+
+'''
+Funzione che permette di riconoscere il viso il real-time
+:param lock: variabile che identifica la funziona di blocco schermo
+:return None
+'''
+def live(lock) -> None:
+
+    cv2.namedWindow('Predicting for')
+    images = []
+    labels = []
+    labels_dic = {}
+    people = []
+    for person in os.listdir("people_folder"):
+        if not person.startswith("."):
+            people.append(person)
+
+    threshold = 80  # Soglia per l'algoritmo di riconoscimento facciale/distanza consentita
+    # da un'altra faccia
+
+    for i, person in enumerate(people):
+
+        labels_dic[i] = person
+        for image in os.listdir("people_folder/" + person):
+
+            images.append(cv2.imread('people_folder/' + person + '/' + image, 0))
+            labels.append(i)
+
+    labels = np.array(labels)
+
+    # rec_eig = cv2.face.EigenFaceRecognizer_create()
+    rec_lbhp = cv2.face.LBPHFaceRecognizer_create()  # Crea un oggetto LBHP face recognizer
+    rec_lbhp.train(images, labels)  # Addestra il modello
+    cv2.namedWindow('face')
+    webcam = cv2.VideoCapture(0)
+    while True:
+
+        _, frame = webcam.read()
+        faces = face_cascade.detectMultiScale(frame, 1.3, 5)  # Prende le coordinate del volto nel frame
+        if len(faces):
+
+            cut_face = cut_faces(frame, faces)  # Taglia la faccia per inserirla nel nostro modello predittivo
+            face = cv2.cvtColor(cut_face[0], cv2.COLOR_BGR2GRAY)
+            face = cv2.equalizeHist(face)  # Equalizzazione Istogramma
+            face = cv2.resize(face, (100, 100), interpolation=cv2.INTER_CUBIC)  # Ridimensiona l'immagine del volto.
+            cv2.imshow('face', face)
+            collector = cv2.face.StandardCollector_create()
+            rec_lbhp.predict_collect(face, collector)
+            conf = collector.getMinDist()  # Trova la faccia più vicina alla nostra (confrontando le distanze e selezionando la minima)
+            print('Confidence ', conf)
+            pred = collector.getMinLabel()
+            if conf < threshold:  # Se un match del volto è trovato
+
+                txt = labels_dic[pred].upper()  # Prendi il nome della persona, maiuscolo
+
+            else:
+
+                txt = 'Sconosciuto'  # Se non riconosciuto, segnalo come sconosciuto
+                if lock: #se lock è vera allora procedo con il blocco schermo
+                    loginPF = CDLL('/System/Library/PrivateFrameworks/login.framework/Versions/Current/login')
+                    result = loginPF.SACLockScreenImmediate()
+
+            cv2.putText(frame, txt, (faces[0][0], faces[0][1] - 10), cv2.FONT_HERSHEY_PLAIN, 3, (66, 53, 243), 2)  # Inserisce il testo nel frame corrente
+            print(faces)
+            cv2.rectangle(frame, (faces[0][0], faces[0][1]), (faces[0][0] + faces[0][2], faces[0][1] + faces[0][3]), (255, 255, 0), 8)  # Crea un rettangolo attorno al volto
+            cv2.putText(frame, "ESC to exit", (5, frame.shape[0] - 10), cv2.FONT_HERSHEY_PLAIN, 1.3, (66, 53, 243), 2, cv2.LINE_AA)
+
+        cv2.imshow("Live", frame)  # Mostra il frame
+
+        if cv2.waitKey(20) & 0xFF == 27:
+
+            destroy()
+            break
+
+'''
+funzione che termina i processi di face recognition
+:param None
+:return None
+'''
+def destroy() -> None:
+
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,207 +1,50 @@
 from faceRecFunction import *
 
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # Oggetto del face detector
-
-roi_gray = []
-
-
-# Rimuove le parti laterali del volto
-
-# Questo è fatto in modo tale che l'algoritmo debba lavorare solo con le parti rilevanti e più importanti dell'immagine
-# Mi raccomando un limite consiste nel registrare il volto frontalmente.
-def cut_faces(image, faces_coord):
-    faces = []
-
-    for (x, y, w, h) in faces_coord:  # Ritaglia parti del viso
-        w_rm = int(0.2 * w / 2)
-        faces.append(image[y: y + h, x + w_rm:  x + w - w_rm])
-
-    return faces  # Restituisce le coordinate della faccia
-
-
-
-
-
-# Effettua la face recognition in real time, premendo ESC chiude la face recognition
-
-def live():
-    cv2.namedWindow('Predicting for')
-    images = []
-    labels = []
-    labels_dic = {}
-    people = []
-    for person in os.listdir("people_folder"):
-        if not person.startswith("."):
-            people.append(person)
-
-    threshold = 80  # Soglia per l'algoritmo di riconoscimento facciale/distanza consentita
-    # da un'altra faccia
-
-    for i, person in enumerate(people):
-
-        labels_dic[i] = person
-
-        for image in os.listdir("people_folder/" + person):
-            images.append(cv2.imread('people_folder/' + person + '/' + image, 0))
-            labels.append(i)
-
-    labels = np.array(labels)
-
-    # rec_eig = cv2.face.EigenFaceRecognizer_create()
-    rec_lbhp = cv2.face.LBPHFaceRecognizer_create()  # Crea un oggetto LBHP face recognizer
-
-    rec_lbhp.train(images, labels)  # Addestra il modellos
-
-    cv2.namedWindow('face')
-    webcam = cv2.VideoCapture(0)
-    while True:
-        _, frame = webcam.read()
-
-        faces = face_cascade.detectMultiScale(frame, 1.3, 5)  # Prende le cordinate del volto nel frame
-
-        if len(faces):
-            cut_face = cut_faces(frame, faces)  # Taglia la faccia per inserirla nel nostro modello predittivo
-
-            face = cv2.cvtColor(cut_face[0], cv2.COLOR_BGR2GRAY)
-            face = cv2.equalizeHist(face)  # Equalizzazione Istogramma
-            face = cv2.resize(face, (100, 100), interpolation=cv2.INTER_CUBIC)  # Ridimensiona l'immagine del volto.
-
-            cv2.imshow('face', face)
-
-            collector = cv2.face.StandardCollector_create()
-            rec_lbhp.predict_collect(face, collector)
-            conf = collector.getMinDist()  # Trova la faccia più vicina alla nostra (confrontando le distanze e selezionando la minima)
-
-            print('Confidence ', conf)
-            pred = collector.getMinLabel()
-            txt = ''
-
-            if conf < threshold:  # Se un match del volto è trovato
-                txt = labels_dic[pred].upper()  # Prendi il nome della persona, maiuscolo
-            else:
-                txt = 'Sconosciuto'  # Se non riconosciuto, segnalo come sconosciuto
-
-
-
-            cv2.putText(frame, txt,
-                        (faces[0][0], faces[0][1] - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 3, (66, 53, 243), 2)  # Inserisce il testo nel frame corrente
-
-            print(faces)
-            cv2.rectangle(frame, (faces[0][0], faces[0][1]), (faces[0][0] + faces[0][2], faces[0][1] + faces[0][3]),
-                          (255, 255, 0), 8)  # Crea un rettangolo attorno al volto
-
-            cv2.putText(frame, "ESC to exit", (5, frame.shape[0] - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 1.3, (66, 53, 243), 2, cv2.LINE_AA)
-
-        cv2.imshow("Live", frame)  # Mostra il frame
-
-        if cv2.waitKey(20) & 0xFF == 27:
-            cv2.destroyAllWindows()
-            break
-
-def lock():
-    cv2.namedWindow('Predicting for')
-    images = []
-    labels = []
-    labels_dic = {}
-    people = []
-    for person in os.listdir("people_folder"):
-        if not person.startswith("."):
-            people.append(person)
-
-    threshold = 80  # Soglia per l'algoritmo di riconoscimento facciale/distanza consentita
-    # da un'altra faccia
-
-    for i, person in enumerate(people):
-
-        labels_dic[i] = person
-
-        for image in os.listdir("people_folder/" + person):
-            images.append(cv2.imread('people_folder/' + person + '/' + image, 0))
-            labels.append(i)
-
-    labels = np.array(labels)
-
-    # rec_eig = cv2.face.EigenFaceRecognizer_create()
-    rec_lbhp = cv2.face.LBPHFaceRecognizer_create()  # Crea un oggetto LBHP face recognizer
-
-    rec_lbhp.train(images, labels)  # Addestra il modellos
-
-    cv2.namedWindow('face')
-    webcam = cv2.VideoCapture(0)
-    while True:
-        _, frame = webcam.read()
-
-        faces = face_cascade.detectMultiScale(frame, 1.3, 5)  # Prende le cordinate del volto nel frame
-
-        if len(faces):
-            cut_face = cut_faces(frame, faces)  # Taglia la faccia per inserirla nel nostro modello predittivo
-
-            face = cv2.cvtColor(cut_face[0], cv2.COLOR_BGR2GRAY)
-            face = cv2.equalizeHist(face)  # Equalizzazione Istogramma
-            face = cv2.resize(face, (100, 100), interpolation=cv2.INTER_CUBIC)  # Ridimensiona l'immagine del volto.
-
-            cv2.imshow('face', face)
-
-            collector = cv2.face.StandardCollector_create()
-            rec_lbhp.predict_collect(face, collector)
-            conf = collector.getMinDist()  # Trova la faccia più vicina alla nostra (confrontando le distanze e selezionando la minima)
-
-            print('Confidence ', conf)
-            pred = collector.getMinLabel()
-            txt = ''
-
-            if conf < threshold:  # Se un match del volto è trovato
-                txt = labels_dic[pred].upper()  # Prendi il nome della persona, maiuscolo
-            else:
-                txt = 'Sconosciuto'  # Se non riconosciuto, segnalo come sconosciuto
-
-                #Le due righe di codice sottostanti, sfruttando il package ctypes permettono di effettuare la lock screen del pc in uso.
-                loginPF = CDLL('/System/Library/PrivateFrameworks/login.framework/Versions/Current/login')
-                result = loginPF.SACLockScreenImmediate()
-
-            cv2.putText(frame, txt,
-                        (faces[0][0], faces[0][1] - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 3, (66, 53, 243), 2)  # Inserisce il testo nel frame corrente
-
-            print(faces)
-            cv2.rectangle(frame, (faces[0][0], faces[0][1]), (faces[0][0] + faces[0][2], faces[0][1] + faces[0][3]),
-                          (255, 255, 0), 8)  # Crea un rettangolo attorno al volto
-
-            cv2.putText(frame, "ESC to exit", (5, frame.shape[0] - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 1.3, (66, 53, 243), 2, cv2.LINE_AA)
-
-        cv2.imshow("Live", frame)  # Mostra il frame
-
-        if cv2.waitKey(20) & 0xFF == 27:
-            cv2.destroyAllWindows()
-            break
-
-
 while True:
+
     print("Hello there please select one of the below")
     print('Press 1 for adding a new face')
     print('Press 2 for the live recognition')
     print('Press 3 to test Lock Screen Functionality')
     print('Press 4 to exit')
-
     choice = int(input())
 
     if choice > 3 or choice < 1 or choice == '':
+
         print('Please select a valid choice')
+
     if choice == 1:
+
         add_person()
+
     elif choice == 2:
-        live()
+
+        live(False)
+
     elif choice == 3:
-        lock()
+
+        live(True)
+
     elif choice == 4:
+
         print('You opted to exit!')
         break
 
-    cv2.destroyAllWindows()
+destroy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
